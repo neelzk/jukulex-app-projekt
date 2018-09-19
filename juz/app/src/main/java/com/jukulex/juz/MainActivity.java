6bc2eb64d45d4dd7fdca963f6026d2138fe41307
Nils <nils.koch@ruhr-uni-bochum.de>
2018-09-19 14:30:07 +0200

    maps activity hinzugefuegt

--- juz/app/src/main/java/com/jukulex/juz/MainActivity.java
+++ juz/app/src/main/java/com/jukulex/juz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jukulex.juz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -92,6 +93,10 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.nav_maps) {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
