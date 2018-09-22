c16793732ceacc9c7676f73663eda589e49bc186
Nils <nils.koch@posteo.de>
2018-09-22 16:09:05 +0200

    echo sql error; indentation

--- server/addmarker.php
+++ server/addmarker.php
@@ -1,30 +1,30 @@
 <?php
-        if (!isset($_POST["text"])) {
-                header('HTTP/1.0 500 Internal Server Error');
-                die();
-        }
-        if (!isset($_POST["longitude"])) {
-                header('HTTP/1.0 500 Internal Server Error');
-                die();
-        }
-        if (!isset($_POST["latitude"])) {
-                header('HTTP/1.0 500 Internal Server Error');
-                die();
-        }
+    if (!isset($_POST["text"])) {
+        header('HTTP/1.0 500 Internal Server Error');
+        die();
+    }
+    if (!isset($_POST["latitude"])) {
+        header('HTTP/1.0 500 Internal Server Error');
+        die();
+    }
+    if (!isset($_POST["longitude"])) {
+        header('HTTP/1.0 500 Internal Server Error');
+        die();
+    }
 
-        require_once("db.php");
+    require_once("db.php");
 
-        /*
-        if (isset($_POST["user"] && isset($_POST["pass"]) {
+    /*
+    if (isset($_POST["user"] && isset($_POST["pass"]) {
 
-        }
-        */
+    }
+    */
 
-        $text = mysqli_real_escape_string($_POST["text"]));
-        $longitude = mysqli_real_escape_string($_POST["longitude"]);
-        $latitude = mysqli_real_escape_string($_POST["latitude"]);
+    $text = $mysqli->real_escape_string($_POST["text"]);
+    $latitude = $mysqli->real_escape_string($_POST["latitude"]);
+    $longitude = $mysqli->real_escape_string($_POST["longitude"]);
 
-        $sql  = 'INSERT INTO `markers` (`id`, `text`, `latitude`, `longitude`) VALUES (NULL, \''.$text.'\', \''.$latitude.'\', \''.$longitude.'\')';
-        $res = $mysqli->query($sql);
+    $sql  = "INSERT INTO `markers` (`id`, `text`, `latitude`, `longitude`) VALUES (NULL, '$text', '$latitude', '$longitude')";
+    $res = $mysqli->query($sql);
 
 ?>
--- server/register.php
+++ server/register.php
@@ -1,59 +1,48 @@
 <?php
-        function dieWithHttpMsg($msg) {
-                echo "$msg\n";
-                die();
-        }
+    function dieWithHttpMsg($msg) {
+        echo "$msg\n";
+        die();
+    }
 
-        require_once('db.php');
+    require_once('db.php');
 
-        $error = NULL;
-        if (!isset($_POST["username"])) {
-                $error['username'] = 'Bitte Benutzername angeben.';
-        }
-        if (!isset($_POST["password"])) {
-                $error['password'] = 'Bitte Passwort angeben.';
-        }
-        if (!isset($_POST["email"])) {
-                $error['email'] = 'Bitte E-Mail angeben.';
-        }
+    $error = NULL;
+    if (!isset($_POST["username"])) {
+        $error['username'] = 'Bitte Benutzername angeben.';
+    }
+    if (!isset($_POST["password"])) {
+        $error['password'] = 'Bitte Passwort angeben.';
+    }
+    if (!isset($_POST["email"])) {
+        $error['email'] = 'Bitte E-Mail angeben.';
+    }
 
-        if ($error != NULL) {
-                $errorMsg = "";
-                foreach ($error as $key => $value) {
-                    $errorMsg .= "Error: $value ";
-                }
-                diewithHttpsMsg($errorMsg);
+    if ($error != NULL) {
+        $errorMsg = "";
+        foreach ($error as $key => $value) {
+            $errorMsg .= "Error: $value ";
         }
+        diewithHttpsMsg($errorMsg);
+    }
 
-        $username = $mysqli->real_escape_string($_POST["username"]);
+    $username = $mysqli->real_escape_string($_POST["username"]);
+    $password = $mysqli->real_escape_string($_POST["password"]);
+    $email = $mysqli->real_escape_string($_POST["email"]);
 
-        $sql = 'SELECT * FROM `users` WHERE `name` = \''.$username.'\'';
-        $res = $mysqli->query($sql);
-        if (!$res) {
-                dieWithHttpMsg("Error: DB Zugriff nicht moeglich.");
-        }
-        if ($res->num_rows > 0) {
-                dieWithHttpMsg("Error: Nutzer existiert bereits.");
-        }
-        
-        $password = $mysqli->real_escape_string($_POST["password"]);
-        $email = $mysqli->real_escape_string($_POST["email"]);
+    $hash = password_hash($password, PASSWORD_DEFAULT);
 
-        $hash = password_hash($password, PASSWORD_DEFAULT);
+    /*
+    if (password_verify('rasmuslerdorf', $hash)) {
+        echo 'Password is valid!';
+    } else {
+        echo 'Invalid password.';
+    }
+    */
 
-        /*
-        if (password_verify('rasmuslerdorf', $hash)) {
-                echo 'Password is valid!';
-        } else {
-                echo 'Invalid password.';
-        }
-        */
+    $sql  = "INSERT INTO `users` (`id`, `gid`, `name`, `email`, `password`) VALUES (NULL, '0', '$username', '$email', '$hash')";
 
-        $sql  = "INSERT INTO `users` (`id`, `gid`, `name`, `email`, `password`) VALUES (NULL, '0', '$username', '$email', '$hash')";
-
-        $res = $mysqli->query($sql);
-        if (!$res) {
-                dieWithHttpMsg("Error: DB Zugriff nicht moeglich.");
-        }
+    $res = $mysqli->query($sql);
+    if (!$res) {
+        dieWithHttpMsg($mysqli->error);
+    }
 ?>
-
