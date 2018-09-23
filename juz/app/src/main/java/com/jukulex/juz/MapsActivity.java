c022f70d195061c68520ca84a5efff3f44a87024
Nils <nils.koch@posteo.de>
2018-09-23 13:33:07 +0200

    funktion addBuiltinMarkers hinzugefuegt; test code fuer eigene position

--- juz/app/src/main/java/com/jukulex/juz/MapsActivity.java
+++ juz/app/src/main/java/com/jukulex/juz/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.jukulex.juz;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,49 +14,118 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class MapsActivity extends SupportMapFragment implements OnMapReadyCallback, OnMapLongClickListener {
 
     private GoogleMap mMap;
 
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+//    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+//        .setTimestampsInSnapshotsEnabled(true)
+//        .build();
+
+
+    private CollectionReference mCollectionRef = firestore.collection("Markers");
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+//        firestore.setFirestoreSettings(settings);
+    }
+
     @Override
     public void onResume() {
         super.onResume();
-
-        setUpMapIfNeeded();
-    }
-
-    private void setUpMapIfNeeded() {
 
         if (mMap == null) {
             getMapAsync(this);
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+            //mMap.setMyLocationEnabled(true);
+        }
+
         mMap.setOnMapLongClickListener(this);
 
-        // Add Häuschen Almena marker and move the camera
-        LatLng haeuschen = new LatLng(52.1058, 9.0823);
-        mMap.addMarker(new MarkerOptions().position(haeuschen).title("Häuschen in Almena"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(haeuschen,15.0f));
+        addBuiltinMarkers();
+
+        fetchMarkersFromDb();
     }
 
     @Override
     public void onMapLongClick(LatLng point) {
         LatLng marker = new LatLng(point.latitude, point.longitude);
         mMap.addMarker(new MarkerOptions().position(marker).title("new Marker"));
+    }
+
+    private void addBuiltinMarkers() {
+
+        // Juz Silixen
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(52.138081, 9.060906))
+                .title("Mehrgenerationenhaus")
+                .snippet("Dietrich-Bonhoeffer-Str. 2, 32699 Extertal")
+        );
+
+        // Juz Bösingfeld
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(52.067692, 9.130269))
+                .title("Cinema 55")
+                .snippet("Mittelstr. 55, 32699 Extertal")
+        );
+
+        // Juz Almena. Center camera around this marker
+        LatLng haeuschen = new LatLng(52.105718, 9.082175);
+        mMap.addMarker(new MarkerOptions()
+                .position(haeuschen)
+                .title("Häuschen")
+                .snippet("Fütiger Str. 34, 32699 Extertal"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(haeuschen, 15.0f));
+
+    }
+
+    private void fetchMarkersFromDb() {
+        mCollectionRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                List<DocumentSnapshot> docSnapshots = queryDocumentSnapshots.getDocuments();
+                for (DocumentSnapshot ds : docSnapshots) {
+                    GeoPoint gp = ds.getGeoPoint("location");
+                    if (gp == null) {
+                        return;
+                    }
+                    String name = ds.getString("name");
+                    if (name == null) {
+                        return;
+                    }
+
+                    //addMarkerToMap(new LatLng(gp.getLatitude(), gp.getLongitude()), name);
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(gp.getLatitude(), gp.getLongitude())).title(name));
+                }
+            }
+
+        });
     }
 }
