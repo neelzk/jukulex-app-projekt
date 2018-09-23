ea84e0e986e017c3f0023852ecde8d31da2f6389
Nils <nils.koch@posteo.de>
2018-09-23 21:58:29 +0200

    login/out mit firebase auth

--- juz/app/src/main/java/com/jukulex/juz/MainActivity.java
+++ juz/app/src/main/java/com/jukulex/juz/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jukulex.juz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,8 +17,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +56,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
 
         HomeFragment homeFrag = new HomeFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFrag).commit();
@@ -88,6 +104,8 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
             HomeFragment homeFrag = new HomeFragment();
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFrag).commit();
+        } else if (id == R.id.nav_loginoff) {
+            onLoginLogoffClicked();
         } else if (id == R.id.nav_events) {
             EventsFragment eventsFrag = new EventsFragment();
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, eventsFrag).commit();
@@ -107,5 +125,50 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 123) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                Log.d("Juzapp Main", "Successfully signed in");
+                FirebaseUser user = mAuth.getCurrentUser();
+                // ...
+            } else {
+                Log.d("Juzapp Main", "Sign in failed: " + response.getError().getMessage());
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    private void onLoginLogoffClicked() {
+        Log.d("Juzapp", "loginlogoffclicked");
+        if (mAuth.getCurrentUser() == null) {
+            Log.d("Juzapp", "current user == null");
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+//TODO:                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build());
+
+            startActivityForResult(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    123);
+        } else {
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Log.d("Juzapp", "logged off");
+                }
+            });
+        }
     }
 }
